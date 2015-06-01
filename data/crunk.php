9ec8bdf36e1065365ce9c0537e8bb59976e8d2a4
James Browning <jamesb.fe80@gmail.com>
2015-06-01 09:44:07 -0700

    adjust link in Redmond to load external tenement instead of the internal one.
    introduce new features to inaccessible Redmond clone for testing.
    add resource pack to dependencies in project c pack
    introduce primitive scene breaking php file
    automatic story file cruft

---
+++ data/crunk.php
@@ -0,0 +1,43 @@
+<?php
+
+$I="run location-telestrian r&d.srt.txt";
+$handle = @fopen($I, "r");
+
+$bub = @fopen("parts/bub", "w");
+$part="";
+$count=111;
+
+$inny=0;
+if ($handle) {
+	while (($buffer = fgets($handle, 4096)) !== false) {
+		if($inny==0) {
+			if(preg_match("|^\S.*\{$|", $buffer)) {
+				// begin new trigger/misc
+				$fname=sprintf("parts/part-%03u", $count++);
+				$part=@fopen($fname, "w");
+				fwrite($part, $buffer);
+				$inny=1;
+			  } else {
+				// this in not an inner
+				fwrite($bub, $buffer);
+			  }
+	          } else {
+			if(preg_match("/^\}$/", $buffer)) {
+				// print the closing bracket of the trigger/misc
+				fwrite($part, $buffer);
+				$inny=0;
+				fclose($part);
+			  } else {
+				// print addtional line in trigger/misc
+				fwrite($part, $buffer);
+			  }
+	          }
+//        echo $buffer;
+	  }
+	if (!feof($handle)) {
+		echo "Error: unexpected fgets() fail\n";
+	  }
+	fclose($handle);
+	fclose($bub);
+  }
+?>
