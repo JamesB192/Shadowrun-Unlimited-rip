c9f90c99dc8006164168bec84679e2d40d83332a
James Browning <JamesB.fe80@gmail.com>
2017-04-11 07:51:01 -0700

    strip tooling

--- SR-Unlimited/data/convos/52463d78336331cc13003c03.php
+++
@@ -1,7 +0,0 @@
-<?php
-$a = file_get_contents("52463d78336331cc13003c03.frag");
-for($x=2;$x<=7;$x++) {
-  printf($a, $x, $x, $x, $x, $x, $x);
- }
-?>
-
--- SR-Unlimited/data/convos/58e04fad3037363b6789d1a7.php
+++
@@ -1,6 +0,0 @@
-<?php
-$a = file_get_contents("58e04fad3037363b6789d1a7.frag");
-for($x=2;$x<=36;$x++) {
-  printf($a, $x, $x, $x, $x, $x, $x);
- }
-?>
