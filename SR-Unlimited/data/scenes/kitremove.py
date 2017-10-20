0a2e831ff7e8e6758c9fb9e56718fc55e429f217
James Browning <JamesB.fe80@gmail.com>
2017-10-20 06:52:45 -0700

    Finish converting Argyle to use the equipment sheet and the tool I (re)wrote for it.

---
+++ SR-Unlimited/data/scenes/kitremove.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import os
+import re
+import stat
+import sys
+
+substitute = """
+"""
+
+splitter = """
+    equipment {
+      prefab_name: "GhoulFist"
+    }
+"""
+
+
+def inner_thing(fbase):
+    result = []
+    try:
+        fp = open(fbase, "r")
+        blob = fp.read(4000000)
+    finally:
+        fp.close
+
+    chunks = blob.split(splitter)
+    for chunk in chunks:
+#        foo = original.search(chunk)
+#        if foo:
+#            result += [substitute]
+#        else:
+            result += [chunk]
+
+#    print(splitter.join(result))
+    try:
+        fp = open(fbase, "w")
+        fp.write(substitute.join(result))
+    finally:
+        fp.close
+
+
+def descend():
+    for f in os.listdir("."):
+        mode = os.stat(f).st_mode
+        if stat.S_ISREG(mode) and True:
+            inner_thing(f)
+
+# fname = "yakuza lab.srm.txt"
+fname = sys.argv[1]
+
+inner_thing(fname)
