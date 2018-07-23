c2f340759cba15ba9e24ad98b9316771af14e2b4
James Browning <jamesb.fe80@gmail.com>
2018-07-23 09:27:31 -0700

    phpexperiment: python blob simulating the wobble of runners reactions to PC0

---
+++ phpexperiment/reactions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Reactions
+# -21,-11 Awful
+# -10,  0 Bad
+#   1,  7 Mediocre
+#   8, 14 Good
+#  15, 21 Excellent
+
+class reaction:
+    the_reaction = 0
+    
+    def update_reaction(self, mod):
+        self.the_reaction = int(self.the_reaction * 19 / 20) + mod
+        print(self.the_reaction)
+
+foo = reaction()
+
+for I in range(30):
+    foo.update_reaction(-2)
+for I in range(30):
+    foo.update_reaction(2)
+for I in range(30):
+    foo.update_reaction(-2)
+
