2197c9bd27a751dadab853db9fddae807985685e
James Browning <jamesb.fe80@gmail.com>
2018-09-04 15:40:09 -0700

    tooling change and make the project r/w again

--- SR-Unlimited/data/scenes/dosubby.py
+++ SR-Unlimited/data/scenes/dosubby.py
@@ -1,63 +1,71 @@
 #!/usr/bin/python
-import dosubby2
+from dosubby2 import original_text_fragment, replacement_text
 import os
 import re
 import stat
-# import sys
 
 
-def inner_thing(fbase):
-    terminate = "\n"
-    splitter = terminate + "}" + terminate
-    result = []
-    try:
-        fp = open(fbase, "r")
-        blob = fp.read(4000000)
-    finally:
-        fp.close
+class dosubby:
+    new = []
+    old = []
+    common = 0
 
-    count = 0
-    uncount = 0
-    chunks = blob.split(splitter)
+    def __init__(self, a, b):
+        self.old = a
+        self.new = b
+        self.common = min(len(a), len(b))
 
-    if len(chunks) == 1:
-        terminate = "\r\n"
-        splitter = terminate + "}" + terminate
-        chunks = blob.split(splitter)
-    if len(chunks) == 1:
-        terminate = "\r"
-        splitter = terminate + "}" + terminate
-        chunks = blob.split(splitter)
+    def fileEdit(self, fname):
+        lenny = 0
+        result = []
+        count = 0
+        uncount = 0
 
-    for chunk in chunks:
-        swapped = False
-        for x in range(6):
-            foo = dosubby2.originala[x].search(chunk)
-            if foo:
-                count += 1
-                result += [dosubby2.substitutea[x]]
-                swapped = True
-        if not swapped:
-            result += [chunk]
-            uncount += 1
-    print('%4d\t%4d\t%4d\t%s' % (count, uncount,
-                                 (len(chunks)-count-uncount), fbase))
-    try:
-        fp = open(fbase, "w")
-        fp.write(splitter.join(result))
-    finally:
-        fp.close
+        try:
+            fp = open(fname, "r")
+            blob = fp.read(4000000)
+        finally:
+            fp.close
 
+        for x in ['\n', '\r\n', '\r']:
+            splitter = x + "}" + x
+            chunks = blob.split(splitter)
+            lenny = len(chunks)
+            if lenny > 1:
+                break
 
-def descend(dir):
-    txt = re.compile('.*\.txt')
-    for f in os.listdir(dir):
-        mode = os.stat(f).st_mode
-        if stat.S_ISREG(mode) and txt.search(f):
-            inner_thing(f)
+        for chunk in chunks:
+            swapped = False
+            for x in range(self.common):
+                foo = self.old[x].search(chunk)
+                if foo:
+                    count += 1
+                    result += [self.new[x]]
+                    swapped = True
+            if not swapped:
+                result += [chunk]
+                uncount += 1
+        print('%4d\t%4d\t%4d\t%s' % (count, uncount,
+                                     (len(chunks)-count-uncount), fname))
+        try:
+            fp = open(fname, "w")
+            fp.write(splitter.join(result))
+        finally:
+            fp.close
+
+    def descend(self, dir):
+        txt = re.compile('.*\.txt')
+        for f in os.listdir(dir):
+            mode = os.stat(f).st_mode
+            if stat.S_ISREG(mode) and txt.search(f):
+                self.fileEdit(f)
 
 
 if __name__ == '__main__':
-#    fname = sys.argv[1]
-#    inner_thing("graveyard.srt.txt")
-    descend(".")
+    from sys import argv
+    foo = dosubby(original_text_fragment, replacement_text)
+    if len(argv) < 2:
+        file = "graveyard.srt.txt"
+    else:
+        file = argv[1]
+    foo.fileEdit(file)
