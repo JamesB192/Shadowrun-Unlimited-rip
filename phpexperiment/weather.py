5f0a7037706a24edc2121432c5f8b4aa44d1f7b1
James Browning <jamesb.fe80@gmail.com>
2022-04-04 00:03:11 -0700

    py weather scaling down

--- phpexperiment/weather.py
+++ phpexperiment/weather.py
@@ -8,47 +8,52 @@ def fakediv(num, den):
     return num / den
 
 
+fmt = "%4s %4s %5s %4s %4s %8s"
+rule = tuple("=== ==== ===== === == =======".split())
+hfoot = tuple("Day Hour Trend dHg Hg SkyView".split())
 skies = ['clear', 'cloudy', 'rainy', 'thundery']
 dHg = 0
 trend = 0
-Hg = 1000
+Hg = 50
 sky = 0
 
-print("x\ttrend\tdHg\tHg")
-print("============================")
-for I in range(30):
-    trend = fakediv((Hg - 985), abs(Hg - 985))
-    trend *= -2
+print(fmt % hfoot)
+print(fmt % rule)
+for Day in range(7):
+    for hour in range(24):
+        trend = int(fakediv((Hg - 35), abs(Hg - 35)))
+        trend *= -2
 
-    dHg += int(trend / random.randint(1, 4))
-    dHg += random.randint(1, 6) - random.randint(1, 6)
-    dHg += random.randint(1, 6) - random.randint(1, 6)
+        dHg += int(trend / random.randint(1, 4))
+        dHg += random.randint(1, 6) - random.randint(1, 6)
+        dHg += random.randint(1, 6) - random.randint(1, 6)
 
-    dHg = (12 + dHg - abs(12 - dHg)) / 2
-    dHg = (-12 + dHg + abs(-12 - dHg)) / 2
+        dHg = int((12 + dHg - abs(12 - dHg)) / 2)
+        dHg = int((-12 + dHg + abs(-12 - dHg)) / 2)
 
-    Hg += dHg
+        Hg += dHg
 
-    Hg = (960 + Hg + abs(960 - Hg)) / 2
-    Hg = (1040 + Hg - abs(1040 - Hg)) / 2
-# ====================================================
-    if (sky == 0):
-        if ((990 > Hg) or ((1010 > Hg) and (random.randint(1, 4) == 1))):
-            sky = 1
-    elif (sky == 1):
-        if (False or ((1030 < Hg) and (random.randint(1, 4) == 1))):
-            sky = 0
-        elif ((970 > Hg) or ((990 > Hg) and (random.randint(1, 4) == 1))):
-            sky = 2
-    elif (sky == 2):
-        if ((1030 < Hg) or ((1010 < Hg) and (random.randint(1, 4) == 1))):
-            sky = 1
-        elif (False or ((970 > Hg) and (random.randint(1, 4) == 1))):
-            sky = 3
-    elif (sky == 3):
-        if ((1010 < Hg) or ((990 < Hg) and (random.randint(1, 4) == 1))):
-            sky = 2
-# ====================================================
-    print("%4d\t%4d\t%4d\t%4d\t%s" % (I, trend, dHg, Hg, skies[sky]))
-print("============================")
+        Hg = int((10 + Hg + abs(10 - Hg)) / 2)
+        Hg = int((90 + Hg - abs(90 - Hg)) / 2)
+        # ====================================================
+        if (sky == 0):
+            if ((40 > Hg) or ((60 > Hg) and (random.randint(1, 4) == 1))):
+                sky = 1
+        elif (sky == 1):
+            if (False or ((80 < Hg) and (random.randint(1, 4) == 1))):
+                sky = 0
+            elif ((20 > Hg) or ((40 > Hg) and (random.randint(1, 4) == 1))):
+                sky = 2
+        elif (sky == 2):
+            if ((80 < Hg) or ((60 < Hg) and (random.randint(1, 4) == 1))):
+                sky = 1
+            elif (False or ((20 > Hg) and (random.randint(1, 4) == 1))):
+                sky = 3
+        elif (sky == 3):
+            if ((60 < Hg) or ((40 < Hg) and (random.randint(1, 4) == 1))):
+                sky = 2
+        # ====================================================
+        print(fmt % (Day, hour, trend, dHg, Hg, skies[sky]))
+    print(fmt % rule)
+print(fmt % hfoot)
 print("")
