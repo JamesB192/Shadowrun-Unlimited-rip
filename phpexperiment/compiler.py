877d7cedbb4a8dcf7dfc50bce12085417edd390f
James Browning <jamesb.fe80@gmail.com>
2026-05-19 02:51:05 -0700

    compiler: port back changes from stableish

--- phpexperiment/compiler.py
+++ phpexperiment/compiler.py
@@ -8,6 +8,7 @@ import argparse
 import glob
 import os
 import shutil
+import sys
 import tempfile
 import zipfile
 import google.protobuf.text_format as tf
@@ -35,6 +36,7 @@ def make_directory(indir, outdir):
     odir1 = odir0 + os.sep + o_name
     os.mkdir(odir1)
     write_file(odir1 + os.sep + "project.cpack.bytes", _proj)
+    shutil.copy2(indir + os.sep + "preview.png", odir1)
 
     idir2 = indir + os.sep + "art"
     odir2 = odir1 + os.sep + "art"
@@ -43,14 +45,14 @@ def make_directory(indir, outdir):
     glb = glob.iglob(
         "%s%s**%s*.png" % (idir2, os.sep, os.sep), recursive=True
     )
-    # glb = glob.glob(odir2, '*.png', recursive=True)
-    for file in glb:
-        # print(repr(file))
-        _entry = proto.ManifestEntry()
+    for num, file in enumerate(glb, start=1):
+        _entry = _man.entries.add()
         _entry.name = file.split(os.sep)[-1]
-        # print(repr(file), repr(odir2 + os.sep + _entry.name))
+        _entry.size = os.stat(file).st_size
         shutil.copy2(file, odir2)
-    write_file(odir2 + os.sep + "manifest.mf.bytes", _proj)
+        sys.stderr.write("art: %d\r" % num)
+    sys.stderr.write("\n")
+    write_file(odir2 + os.sep + "manifest.mf.bytes", _man)
 
     idir2 = indir + os.sep + "data"
     odir2 = odir1 + os.sep + "data"
@@ -76,29 +78,34 @@ def make_directory(indir, outdir):
             os.mkdir(odir3)
         except FileExistsError:
             pass
-        _man = proto.Manifest()
         glb = glob.iglob(
             "%s%s**%s*.%s.txt" % (idir2, os.sep, os.sep, stem),
             recursive=True,
         )
-        for file in glb:
-            _entry = proto.ManifestEntry()
-            _entry.name = subdir + os.sep + file.split(os.sep)[-1]
+        for num, file in enumerate(glb, start=1):
             this_file = ptype()
             parse_file(file, this_file)
-            write_file(odir2 + os.sep + _entry.name, this_file)
-    write_file(odir2 + os.sep + "manifest.mf.bytes", _proj)
+            out_file = odir3 + os.sep + file.split(os.sep)[-1][:-3] + "bytes"
+            write_file(out_file, this_file)
+            _entry = _man.entries.add()
+            _entry.name = os.sep.join(os.sep.split(out_file)[-3:])
+            _entry.size = os.stat(out_file).st_size
+            sys.stderr.write("%s: %d\r" % (stem, num))
+        sys.stderr.write("\n")
+    write_file(odir2 + os.sep + "manifest.mf.bytes", _man)
 
     with zipfile.ZipFile(
         outdir + os.sep + o_name + ".cpz", "w"
     ) as myzip:
         cwd = os.getcwd()
-        os.chdir(odir1)
-        for line in glob.iglob("**", recursive=True):
-            print(line)
+        os.chdir(odir0)
+        manifest = list(glob.iglob("**", recursive=True))
+        for num, line in enumerate(sorted(manifest), start=1):
             myzip.write(line)
+            sys.stderr.write("cpz: %d\r" % num)
+        sys.stderr.write("\n")
         os.chdir(cwd)
-    shutil.rmtree(odir0)
+    #shutil.rmtree(odir0)
 
 
 if "__main__" == __name__:
